e234b7bf2a7c048b1969d16123f0ec60eac00fa8
jackh726 <git@jackhuey.me>
2024-11-18 04:39:07 +0000

    Impl all IrPrint using Debug

--- crates/hir-ty/src/next_solver/ir_print.rs
+++ crates/hir-ty/src/next_solver/ir_print.rs
@@ -4,42 +4,42 @@ use super::interner::DbInterner;
 
 impl IrPrint<ty::AliasTy<Self>> for DbInterner {
     fn print(t: &ty::AliasTy<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(t: &ty::AliasTy<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 
 impl IrPrint<ty::AliasTerm<Self>> for DbInterner {
     fn print(t: &ty::AliasTerm<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(t: &ty::AliasTerm<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::TraitRef<Self>> for DbInterner {
     fn print(t: &ty::TraitRef<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(t: &ty::TraitRef<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::TraitPredicate<Self>> for DbInterner {
     fn print(t: &ty::TraitPredicate<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::TraitPredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<rustc_type_ir::HostEffectPredicate<Self>> for DbInterner {
@@ -47,14 +47,14 @@ impl IrPrint<rustc_type_ir::HostEffectPredicate<Self>> for DbInterner {
         t: &rustc_type_ir::HostEffectPredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &rustc_type_ir::HostEffectPredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::ExistentialTraitRef<Self>> for DbInterner {
@@ -62,14 +62,14 @@ impl IrPrint<ty::ExistentialTraitRef<Self>> for DbInterner {
         t: &ty::ExistentialTraitRef<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::ExistentialTraitRef<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::ExistentialProjection<Self>> for DbInterner {
@@ -77,14 +77,14 @@ impl IrPrint<ty::ExistentialProjection<Self>> for DbInterner {
         t: &ty::ExistentialProjection<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::ExistentialProjection<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::ProjectionPredicate<Self>> for DbInterner {
@@ -92,26 +92,26 @@ impl IrPrint<ty::ProjectionPredicate<Self>> for DbInterner {
         t: &ty::ProjectionPredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::ProjectionPredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::NormalizesTo<Self>> for DbInterner {
     fn print(t: &ty::NormalizesTo<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::NormalizesTo<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::SubtypePredicate<Self>> for DbInterner {
@@ -119,34 +119,34 @@ impl IrPrint<ty::SubtypePredicate<Self>> for DbInterner {
         t: &ty::SubtypePredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::SubtypePredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::CoercePredicate<Self>> for DbInterner {
     fn print(t: &ty::CoercePredicate<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(
         t: &ty::CoercePredicate<Self>,
         fmt: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
 impl IrPrint<ty::FnSig<Self>> for DbInterner {
     fn print(t: &ty::FnSig<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 
     fn print_debug(t: &ty::FnSig<Self>, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        std::fmt::Debug::fmt(t, fmt)
     }
 }
